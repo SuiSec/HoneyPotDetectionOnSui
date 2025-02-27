0f29539661899fd549427962eb7247c9e3ba9510
Randy Pen <pzm16@tsinghua.org.cn>
2025-02-28 07:48:24 +0800

    v 0.0.8

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ const wl: string[] = [
     '0xf16e6b723f242ec745dfd7634ad072c42d5c1d9ac9d62a39c381303eaa57693a::fdusd::FDUSD',   // FDUSD
     '0x960b531667636f39e85867775f52f6b1f220a058c4de786905bdf761e06a56bb::usdy::USDY',     // Ondo USDY
     '0x294de7579d55c110a00a7c4946e09a1b5cbeca2592fbb83fd7bfacba3cfeaf0e::drf::DRF',       // DRF
+    '0x3e8e9423d80e1774a7ca128fccd8bf5f1f7753be658c5e645929037f7c819040::lbtc::LBTC',     // Lombard
     '0xb45fcfcc2cc07ce0702cc2d229621e046c906ef14d9b25e8e4d25f6e8763fef7::send::SEND'
 ];
 
